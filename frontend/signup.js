5eecfa7badf78fa029842bbe5b93757f8cc7172e
Roselle Ardosa <rardosa@uci.edu>
2019-11-02 16:24:01 -0700

    signup connect backend
    Tried to connect the signup pages to the backend database

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,5 +1,10 @@
 function sendEmail()
 {
-    var xhttp = new XMLHttpRequest();
-    
+    $('#form-container').on('submit', function(e){
+        e.preventDefault();
+        var details = $('#form-container').serialize();
+        $.post('http://127.0.0.1:8000/idm/register', details, function(data){
+            console.log(data);
+        })
+    })
 }
