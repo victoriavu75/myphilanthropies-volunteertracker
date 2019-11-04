18e656fd4b0d7c3622427c80d683959993e2fc79
Roselle Ardosa <rardosa@uci.edu>
2019-11-04 14:41:13 -0800

    uwah pt.3
    Changed the regex for in the input
    Changed the .onClick() so it doesn't send multiple requests anymore

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,9 +1,10 @@
 function sendEmail() {
-    $('#form-container').on('submit', function (e) {
+    $('#form-container').unbind().on('submit', function (e) {
         e.preventDefault();
         var details = $('#form-container').serialize();
         $.post('http://127.0.0.1:8000/idm/register', details, function (data) {
             console.log(data);
+        //Make separate alerts for response codes
         })
     })
 }
