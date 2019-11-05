fa23bf756449f1cc69188a1c1651936d0d6a6337
Roselle Ardosa <rardosa@uci.edu>
2019-11-05 09:34:36 -0800

    Update signup.js
    Changed the local host into the aws

--- frontend/signup.js
+++ frontend/signup.js
@@ -2,7 +2,7 @@ function sendEmail() {
     $('#form-container').unbind().on('submit', function (e) {
         e.preventDefault();
         var details = $('#form-container').serialize();
-        $.post('http://127.0.0.1:8000/idm/register', details, function (data) {
+        $.post('http://ec2-3-15-201-67.us-east-2.compute.amazonaws.com/idm/register', details, function (data) {
             console.log(data);
         //Make separate alerts for response codes
         })
