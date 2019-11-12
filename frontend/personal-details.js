f1b80483635ccf8f1b6cdcc7915421567ef225fd
Roselle Ardosa <rardosa@uci.edu>
2019-11-12 10:59:22 -0800

    uwah pt.6
    Added error messages to the sign up for user respones
    Added in jqueries to update JSON object to send to personal data server

--- frontend/personal-details.js
+++ frontend/personal-details.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    sendPersonalDetails();
     $('button').on('click', function(){
         if ($(this).hasClass('select-multiple')){
             if ($(this).hasClass('is-link')) {
@@ -12,3 +13,65 @@ $(document).ready(function(){
         }
     });
   });
+//JSON objects to send to the server
+var personalInfo = {
+    "first_name": "",
+    "last_name": "",
+    "middle_init": "",
+    "gender": "Prefer not to say",
+    "age_range": 0,
+    "job_status": "",
+    "driver": "",
+    "reliable_transportation": ""
+};
+
+//First replace empty strings with information from the user inputs
+//Then, on click of the "submit", send the information to the server.
+function sendPersonalDetails()
+{
+    $('#first-name').change(function()
+    {
+        personalInfo["first_name"] = this.value;
+    });
+    $('#last-name').change(function()
+    {
+        personalInfo["last_name"] = this.value;
+    });
+    $('#middle-initial').change(function()
+    {
+        personalInfo["middle_init"] = this.value;
+    });
+    $('#student').click(function()
+    {
+        personalInfo.job_status += "Student ";
+    });
+    $('#full-time').click(function()
+    {
+        personalInfo.job_status += "Full-Time ";
+    });
+    $('#part-time').click(function()
+    {
+        personalInfo.job_status += "Part-Time ";
+    });
+    $('#unemployed').click(function()
+    {
+        personalInfo.job_status += "Unemployed ";
+    });
+    if ($('#checkbox-1').prop("checked"))
+    {
+        personalInfo.reliable_transportation = "Y";
+    }
+    else
+    {
+        personalInfo.reliable_transportation = "N";
+    }
+    if ($('#checkbox-2').prop("checked"))
+    {
+        personalInfo.driver = "Y";
+    }
+    else
+    {
+        personalInfo.driver = "N";
+    }
+    console.log(personalInfo);
+}
--- frontend/signup.js
+++ frontend/signup.js
@@ -4,8 +4,45 @@ function sendEmail() {
         var details = $('#form-container').serialize();
         $.post('http://ec2-3-15-201-67.us-east-2.compute.amazonaws.com/idm/register', details, function (data) {
             console.log(data);
-            return;
         //Make separate alerts for response codes
+            if (data.message == "JSON Mapping Exception.")
+            {
+                console.log("Problem with connecting endpoints.");
+                document.getElementById("endPointError").style.display = "block";
+                document.getElementById("serverError").style.display = "none";
+                document.getElementById("userAlreadyExists").style.display = "none";
+                document.getElementById("successRegister").style.display = "none";
+                return;
+            }
+            else if (data.message == "Internal server error.")
+            {
+                console.log("Something wrong with server.");
+                document.getElementById("endPointError").style.display = "none";
+                document.getElementById("serverError").style.display = "block";
+                document.getElementById("userAlreadyExists").style.display = "none";
+                document.getElementById("successRegister").style.display = "none";
+                return;
+            }
+            else if (data.message == "User already exists.")
+            {
+                console.log("There's a user already associated with this email.");  
+                document.getElementById("endPointError").style.display = "none";
+                document.getElementById("serverError").style.display = "none";
+                document.getElementById("userAlreadyExists").style.display = "block";
+                document.getElementById("successRegister").style.display = "none";
+                return;
+            }
+            else
+            {
+                console.log("User registered successfully.");
+                document.getElementById("endPointError").style.display = "none";
+                document.getElementById("serverError").style.display = "none";
+                document.getElementById("userAlreadyExists").style.display = "none";
+                document.getElementById("successRegister").style.display = "block";
+                return;
+            }
         })
     })
 }
+
+
