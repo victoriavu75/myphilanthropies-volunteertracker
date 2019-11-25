7ba73eb2e8612a169501a6905f00e2cb60c64995
Roselle Ardosa <rardosa@uci.edu>
2019-11-24 18:59:22 -0800

    owo pt.3
    Made some changes to admin side and added functionality to personal details

--- frontend/profile_home.js
+++ frontend/profile_home.js
@@ -1,12 +1,13 @@
 $(document).ready(function()
 {
     loadUserInfo();
+    //Add in function to set the picture 
+    loadEventInfo();
 });
 
 function loadUserInfo()
 {
     var authToken = localStorage.getItem('token');
-    var userInfo;
     $.ajax({
         type: "GET",
         url: "http://ec2-3-15-201-67.us-east-2.compute.amazonaws.com/volunteer/personalinfo/",
@@ -16,7 +17,7 @@ function loadUserInfo()
             },
         success: function(result)
             {
-                console.log(result);
+                return setUserInfo(result);
             }
     });
     $.ajax({
@@ -24,7 +25,30 @@ function loadUserInfo()
         url: "http://ec2-3-15-201-67.us-east-2.compute.amazonaws.com/volunteer/contactinfo/",
         beforeSend: function(XMLHttpRequest)
             {
-                XMLHttpRequest.setRequestHeader('Authorization', "Token b0dd33181923948af6d2eddf025519d44057f236");
+                XMLHttpRequest.setRequestHeader('Authorization', "Token "+authToken);
+            },
+        success: function(result)
+            {
+                console.log(result);
+            }
+    });
+}
+
+function setUserInfo(response)
+{
+    console.log(response);
+   $("#volunteer-name").html(response[response.length-1].first_name);
+}
+
+function loadEventInfo()
+{
+    var authToken = localStorage.getItem('token');
+    $.ajax({
+        type: "GET",
+        url: "http://ec2-3-15-201-67.us-east-2.compute.amazonaws.com//peinfo/events",
+        beforeSend: function(XMLHttpRequest)
+            {
+                XMLHttpRequest.setRequestHeader('Authorization', "Token "+authToken);
             },
         success: function(result)
             {
